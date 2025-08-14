eb38a2c121e93b94c9554feaf10fd1186858bccc
nickconnelly10 <nickconnelly10@gmail.com>
2025-08-14 14:29:34 -0400

    Update colors and fonts to match main muscadine.box for consistency

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,15 +7,27 @@ export default {
   theme: {
     extend: {
       colors: {
+        'roman-gold': '#D4AF37',
+        'roman-red': '#8B0000',
+        'roman-bronze': '#CD7F32',
+        'roman-marble': '#F5F5DC',
+        'roman-stone': '#696969',
         gold: {
-          600: '#FFC300',
-          700: '#FFB300',
+          50: '#fdf6e3',
+          100: '#f9eec2',
+          200: '#f5e49a',
+          300: '#f1d96e',
+          400: '#ecc94b',
+          500: '#d4af37',
+          600: '#bfa12f',
+          700: '#a68c28',
+          800: '#8c7621',
+          900: '#6e5a17',
         },
-        stone: require('tailwindcss/colors').stone,
       },
       fontFamily: {
-        serif: ['Playfair Display', 'serif'],
-        sans: ['Inter', 'sans-serif'],
+        'serif': ['Georgia', 'serif'],
+        'display': ['Playfair Display', 'serif'],
       },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
@@ -24,4 +36,4 @@ export default {
     },
   },
   plugins: [],
-} 
+}
